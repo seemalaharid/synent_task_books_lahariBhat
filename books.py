88f18d082d2d549ea07b387addcdc1997ef9e335
seemalaharid <bhatlahari01@gmail.com>
2026-05-27 14:37:30 +0530

    Add files via upload

---
+++ books.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+url = "https://books.toscrape.com/"
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+books = soup.find_all("article", class_="product_pod")
+
+with open("books.csv", "w", newline="", encoding="utf-8") as file:
+    writer = csv.writer(file)
+    
+    writer.writerow(["Book Title", "Price"])
+
+    for book in books:
+        title = book.h3.a["title"]
+        price = book.find("p", class_="price_color").text
+
+        writer.writerow([title, price])
+
+        print(title, "-", price)
+
+print("\nData saved to books.csv")
